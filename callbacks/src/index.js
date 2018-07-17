321914a089e9da37961057ebfee5abf30bbd6e82
GContaldi <gaetanocontaldi000@gmail.com>
2018-07-17 20:30:53 +0200

    add set post

--- callbacks/src/index.js
+++ callbacks/src/index.js
@@ -1,13 +1,31 @@
 import Posts from './posts';
 
-const displayPosts = () => {
+const displayPosts = (callback) => {
   Posts.getPosts(function(posts) {
     let output = '';
     posts.forEach((post) => {
       output += `<li>${post.title}</li>`
     });
     document.getElementById('posts').innerHTML = output;
+    if (callback) { callback(posts) };
   });
 };
 
+const addPost = () => {
+  const titleDOM = document.getElementById('title');
+  const contentDOM = document.getElementById('content')
+
+  const title = titleDOM.value;
+  const content = contentDOM.value;
+
+  Posts.setPost({ title, content }, function() {
+    displayPosts(() => {
+      titleDOM.value = '';
+      contentDOM.value = '';
+    });
+  });
+};
+
+
 displayPosts();
+document.getElementById('add-post').addEventListener('click', addPost);
